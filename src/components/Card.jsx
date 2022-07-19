fe8605b87ee799f06dab5a60b6220dc14633ed64
Jorge Fernandez <jorgelfc1989@gmail.com>
2022-07-19 16:46:08 -0400

    added a new component Avatar

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Avatar from "./Avatar";
 
 function Card(props) {
   return (
@@ -6,7 +7,7 @@ function Card(props) {
       <div className="card">
         <div className="top">
           <h2 className="name">{props.name}</h2>
-          <img className="circle-img" src={props.imgURL} alt="avatar_img" />
+          <Avatar imgURL = {props.imgURL}/>
         </div>
         <div className="bottom">
           <p className="info">{props.phone}</p>
