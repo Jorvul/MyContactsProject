a34799b011ab23fdd388b4f1b398edd213cbe7c9
Jorge Fernandez <jorgelfc1989@gmail.com>
2022-07-20 14:14:32 -0400

    erased some comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,24 +22,6 @@ function App() {
       <h1 className="heading">My Contacts</h1>
       <Avatar imgURL="../images/me.jpg" alt="myself" />
       {contacts.map(createCard)}
-      {/* <Card
-        name={contacts[0].name}
-        imgURL={contacts[0].imgURL}
-        phone={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        imgURL={contacts[1].imgURL}
-        phone={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        imgURL={contacts[2].imgURL}
-        phone={contacts[2].phone}
-        email={contacts[2].email}
-      /> */}
     </div>
   );
 }
